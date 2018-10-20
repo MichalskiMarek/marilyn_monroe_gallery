500368c451ae62fcbb2cc1d7971c286514ed651b
Marek <michalskimarek89@gmail.com>
2018-10-20 14:52:23 +0200

    add article component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 import './App.scss';
 import Navigation from "../Navigation/Navigation";
 import {library} from '@fortawesome/fontawesome-svg-core'
-import {faUser, faImage } from '@fortawesome/free-solid-svg-icons'
+import {faUser, faImage, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import Main from "../Main/Main";
 
-
-library.add(faUser);
-library.add(faImage);
+library.add(faUser,faImage,faMapMarkerAlt);
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Navigation/>
+                <Main/>
             </div>
         );
     }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import './Main.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+class Main extends Component {
+    render() {
+        return (
+            <article className={'article'}>
+                <section className={'titleSection'}>
+                    <h1 className="title">Marilyn Monroe</h1>
+                    <p className={'location'}><FontAwesomeIcon icon="map-marker-alt" className={'map-marker-alt'}/>Poznań,
+                        PL</p>
+                </section>
+                <section className={'text'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequuntur cumque cupiditate eaque eius eligendi eos excepturi expedita, inventore nisi non nostrum sapiente tenetur vel voluptatem. Amet, delectus, iure! Et.
+                </section>
+                <blockquote className={'quote'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet, impedit inventore iure obcaecati perspiciatis quos repellat temporibus.
+                </blockquote>
+                <section className={'text'}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequuntur cumque cupiditate eaque eius eligendi eos excepturi expedita, inventore nisi non nostrum sapiente tenetur vel voluptatem. Amet, delectus, iure! Et.
+                </section>
+            </article>
+
+        );
+    }
+}
+
+export default Main;
