b7ffe1f72361d3b4fb430d4afd66afa25fadff2e
Marek <michalskimarek89@gmail.com>
2018-10-22 17:11:42 +0200

    add scale effect on hover of the photo and template strings in url

--- src/actions/photoAction.js
+++ src/actions/photoAction.js
@@ -2,7 +2,10 @@ import {FETCH_PHOTOS} from "./types";
 import axios from "axios";
 
 export const fetchPhotos = () => dispatch => {
-    axios.get('https://api.flickr.com/services/feeds/photos_public.gne?tags=marilyn,monroe&format=json&nojsoncallback=true')
+    const tags = ['marilyn', 'monroe'];
+    const format = 'json';
+    const noJSONcallback = 'true';
+    axios.get(`https://api.flickr.com/services/feeds/photos_public.gne?tags=${tags.join(',')}&format=${format}&nojsoncallback=${noJSONcallback}`)
         .then(response => {
             dispatch({
                 type: FETCH_PHOTOS,
